aa3e3c97451a44f9286ec9643aa45e3a10a0717d
collinsnyamao <collins.nyamao@strathmore.edu>
2019-12-03 11:37:25 +0300

    feat: added test cred

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Mpesa = require('mpesa-node');
-const mpesaApi = new Mpesa({ consumerKey: 'ykI0Bl8Rg9i3pKr1xmtphbxHALxi0Z9E', consumerSecret: '9f1GyUEzKEXyXgKa', shortCode: '600111' });
+const mpesaApi = new Mpesa({
+    consumerKey: 'ykI0Bl8Rg9i3pKr1xmtphbxHALxi0Z9E',
+    consumerSecret: '9f1GyUEzKEXyXgKa',
+    environment: 'sandbox',
+    shortCode: '600172',
+    initiatorName: 'testapi113',
+    lipaNaMpesaShortCode: 174379,
+    lipaNaMpesaShortPass: 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
+    securityCredential: 'Safaricom007@'
+});
 
 
 
@@ -23,7 +32,7 @@ mpesaApi
     .c2bSimulate(
         254715024545,
         5,
-        'h6dk0Ue2'
+        'test'
     )
     .then((result) => {
         //do something
